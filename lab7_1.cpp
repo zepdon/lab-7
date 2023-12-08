40914b8f2b881df33fe5ad2eb9430d38878794fe
Poon <zeepengineer@gmail.com>
2023-12-08 15:13:39 +0700

    lab7_1 completed

--- lab7_1.cpp
+++ lab7_1.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    int a  =  20; b = 30;
-    int c = 4, output;
+    double a  =  20, b = 30, c=4;
+    double output;
 
-    outpot = a - b/c;
-    count << output: << output << \n;
+    output = a - b/c;
+    cout << "output: " << output <<'\n';
     return 0;
 }
