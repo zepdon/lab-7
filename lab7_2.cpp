d52305bf7cd528ad106e8187cd3fd842a246dac4
Poon <zeepengineer@gmail.com>
2023-12-12 12:09:15 +0700

    lab7_2 completed

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -1,5 +1,6 @@
 //Dialog of the first example is given below.
 
+
 /*
 Fahsai: Sawadee ka...Can you tell me your name?
 ?????: Luke Skywalker
@@ -16,3 +17,40 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+string name;
+string movie;
+string date;
+string quote;
+int ID;
+
+int main()
+{
+    cout << "Fahsai: Sawadee ka...Can you tell me your name?\n";
+    cout << "?????: ";
+    getline(cin,name);
+    cout << "Fahsai: Wow!!! "<<name<< " is a really cool name.\n";
+    cout << "Fahsai: I think you are an Engineering student. What is your student ID?\n";
+    cout << name << ": ";
+    cin >> ID;
+    cin.ignore();
+    cout << "Fahsai: I think you may be GEAR "<< ID/10000000-12 <<". I have a free movie ticket for you.\n";
+    cout << "Fahsai: Let's go to the cinema together!!!\n";
+    cout << "Fahsai: What movie do you want to watch?\n";
+    cout << name <<": ";
+    getline(cin,movie);
+    //cout << movie <<endl;
+    cout << "Fahsai: So....which day are you free to go with me?\n";
+    cout << name <<": ";
+    getline(cin,date);
+    //cout << date <<endl;
+    cout << "Fahsai: "<< date <<"....that is OK!!! I'm looking forward to watching "<< movie <<" with you.\n";
+    cout << name <<": ";
+    getline(cin,quote);
+    //cout<<": "<<quote<<endl;
+    cout << "Fahsai: 555+ see you "<<date<<". Bye Bye "<<"\\"<<"(^ ^)/"<<endl;
+}
